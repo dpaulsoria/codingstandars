364bfe2cf4359609bfc0ecd3fe1d69d6b6e0aabe
Paul Soria <dsoria@espol.edu.ec>
2023-05-31 21:57:25 -0500

    Test

---
+++ src/main/java/dpaulsoria/Main.java
@@ -0,0 +1,17 @@
+package dpaulsoria;
+
+/**
+ * Clase principal que contiene el método de inicio del programa.
+ */
+public class Main {
+
+  /**
+   * Punto de entrada principal del programa.
+   *
+   * @param args Los argumentos de línea de comandos pasados al programa.
+   */
+  public static void main(String[] args) {
+    // TODO: Código de inicio del programa
+    System.out.println("Vales verga!");
+  }
+}
